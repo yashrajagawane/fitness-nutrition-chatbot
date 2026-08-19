e628cc0f678a3c71ac428e06524da5a3faed110c
yashrajagawane <agawaneyash865@gmail.com>
2026-08-20 01:23:34 +0530

    feat: integrate mobile install experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { PwaRegister } from "./components/PwaRegister";
+import { PwaInstallPrompt } from "./components/PwaInstallPrompt";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,6 +49,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <PwaRegister />
+        <PwaInstallPrompt />
         {children}
       </body>
     </html>
